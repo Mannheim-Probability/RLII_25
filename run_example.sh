6d1d66bc602fbf27b165e7995c6d2ee454d1aac3
BommeHD <bndkwle@gmail.com>
2025-09-12 11:14:08 +0200

    run_example executable in venv

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+source venv/bin/activate
+
 python train.py --algo "qrdqn" --env AsterixNoFrameskip-v4 --hyperparams buffer_size:100 n_timesteps:1000 --eval-freq 100 --log-interval 100 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1
