3e63d854d23d40123e90f4e3eb0c97e30fdd86c0
Tilman Aach <tilman.aach@web.de>
2025-09-12 14:46:26 +0200

    personalizes run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source venv/bin/activate
+source .venv/bin/activate
 
 python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000 --eval-freq 100 --log-interval 100 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1
+
+#python enjoy.py --algo "ppo" --env AsterixNoFrameskip-v4 -f logs/example_project/example_1
