c628743d7a0054261893f422c2560a592143b682
DanielSchmidt5 <Dschmidt00@gmx.de>
2025-10-14 17:59:30 +0200

    merging custom ppo moritz

--- rl_zoo3/custom_algos/__init__.py
+++ rl_zoo3/custom_algos/__init__.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 from .discounted_ppo import DISCOUNTED_PPO
-
-__all__ = [
-    "DISCOUNTED_PPO",
-]
-=======
 from .ppo_corrected import PPOCorrected
 from .ppo_corrected_2 import PPOCorrected2
 
 __all__ = [
+    "DISCOUNTED_PPO",
     "PPOCorrected",
     "PPOCorrected2",
 ]
->>>>>>> origin/moritz
