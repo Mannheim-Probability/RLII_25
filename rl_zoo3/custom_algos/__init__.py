3bb36ca065a4f3d5d547cf66f6d66f03d2688ba8
DanielSchmidt5 <Dschmidt00@gmx.de>
2025-10-14 17:59:30 +0200

    merging custom ppo moritz

--- rl_zoo3/custom_algos/__init__.py
+++ rl_zoo3/custom_algos/__init__.py
@@ -3,6 +3,7 @@ from .ppo_corrected_2 import PPOCorrected2
 from .PPO_changed_before_Normalization import PPO_changed_before_Normalization
 
 __all__ = [
+    "DISCOUNTED_PPO",
     "PPOCorrected",
     "PPOCorrected2",
     "PPO_changed_before_Normalization",
