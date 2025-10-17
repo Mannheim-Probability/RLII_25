9003df3ce3c8c390f7ad1df670c39ed794bac20d
dschm <sdanie06@students.uni-mainz.de>
2025-10-17 13:30:33 +0200

    loading hyperparams without wandb

--- run_example_load_hyperparams_opt.sh
+++ run_example_load_hyperparams_opt.sh
@@ -2,4 +2,4 @@
 
 source venv/bin/activate
 
-python train.py --algo ppo  --env CartPole-v1 -n 30000  --study-name test_study --storage logs/test_study.log --wandb-project-name RL_2025 -tags test --track --eval-freq 1000 
+python train.py --algo ppo  --env CartPole-v1 -n 30000  --study-name test_study --storage logs/test_study.log --eval-freq 1000 
