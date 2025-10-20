62cb1cec9d35ef7f3a0b196c1cc7932a999c75cc
ahuckema <huckemann57@gmail.com>
2025-10-20 14:22:41 +0200

    ppo_changed_before_normalization added

--- rl_zoo3/custom_algos/__init__.py
+++ rl_zoo3/custom_algos/__init__.py
@@ -1,7 +1,9 @@
 from .ppo_corrected import PPOCorrected
 from .ppo_corrected_2 import PPOCorrected2
+from .PPO_changed_before_Normalization import PPO_changed_before_Normalization
 
 __all__ = [
     "PPOCorrected",
     "PPOCorrected2",
+    "PPO_changed_before_Normalization",
 ]
