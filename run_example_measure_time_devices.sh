893c1a03ec5d6975629cedb08a6da9965dc007e2
BommeHD <bndkwle@gmail.com>
2025-10-30 14:41:59 +0100

    Runtime test modified

--- run_example_measure_time_devices.sh
+++ run_example_measure_time_devices.sh
@@ -12,6 +12,6 @@ run_and_time() {
     echo "$name took: ${elapsed}s"
 }
 
-run_and_time "Execution on CPU" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000000 --eval-freq 50000 --log-interval 5000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1 --device cpu
-run_and_time "Execution on GPU" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000000 --eval-freq 50000 --log-interval 5000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1 --device mps
-run_and_time "Execution on automatic device" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:10000 --eval-freq 5000 --log-interval 5000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1
+run_and_time "Execution on CPU" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000000 --eval-freq 50000 --log-interval 50000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1 --device cpu
+run_and_time "Execution on GPU" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000000 --eval-freq 50000 --log-interval 50000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1 --device mps
+run_and_time "Execution on automatic device" python train.py --algo "ppo" --env AsterixNoFrameskip-v4 --hyperparams n_timesteps:1000000 --eval-freq 50000 --log-interval 50000 --eval-episodes 5 --n-eval-envs 2 --seed 1 -f logs/example_project/example_1 --verbose 1
