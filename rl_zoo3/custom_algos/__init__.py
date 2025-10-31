25504d973d5d067d11c45f7a90a489ced6df342f
Moritz Melcher <82319873+moritzmelcher@users.noreply.github.com>
2025-10-31 15:03:38 +0100

    implement ppo variant with finite-horizon GAE

--- rl_zoo3/custom_algos/__init__.py
+++ rl_zoo3/custom_algos/__init__.py
@@ -2,10 +2,12 @@ from .ppo_corrected import PPOCorrected
 from .ppo_corrected_2 import PPOCorrected2
 from .PPO_changed_before_Normalization import PPO_changed_before_Normalization
 from .ppo_corrected_3 import PPOCorrected3
+from .ppo_corrected_4 import PPOCorrected4
 
 __all__ = [
     "PPOCorrected",
     "PPOCorrected2",
     "PPO_changed_before_Normalization",
     "PPOCorrected3",
+    "PPOCorrected4",
 ]
